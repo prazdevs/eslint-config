6bb516a6c21beed2097410ba9e334284e2cd2484
PraZ <sachabouillez@gmail.com>
2021-07-06 23:23:01 +0200

    :construction: restore rule

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -16,10 +16,6 @@ module.exports = {
       parserOptions: {
         parser: '@typescript-eslint/parser',
       },
-      rules: {
-        // 'no-unused-vars': 'off',
-        // '@typescript-eslint/no-unused-vars': 'off',
-      },
     },
   ],
 }
