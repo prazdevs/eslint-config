25439609446afcd24e609ffee22f7d42224ceb01
prazdevs <sachabouillez@gmail.com>
2021-07-09 23:01:47 +0200

    :sparkles: add prettier formatting

---
+++ packages/global/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    '@prazdevs/eslint-config-vue',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        proseWrap: 'always',
+        quoteProps: 'consistent',
+        semi: false,
+        singleQuote: true,
+      },
+    ],
+  },
+}
