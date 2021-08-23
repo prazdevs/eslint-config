5d8c4788c493fd080453b7c90c7da632a9897c68
PraZ <sachabouillez@gmail.com>
2021-08-23 20:14:43 +0200

    :sparkles: add more vue rules

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -8,8 +8,26 @@ module.exports = {
     '@prazdevs/eslint-config-typescript',
   ],
   rules: {
+    'vue/attributes-order': ['error', { alphabetical: true }],
+    'vue/block-tag-newline': [
+      'error',
+      { singleline: 'always', multiline: 'always' },
+    ],
     'vue/max-attributes-per-line': ['error', { singleline: 2 }],
     'vue/max-len': 'error',
+    'vue/no-empty-component-block': 'error',
+    'vue/no-export-in-script-setup': 'error',
+    'vue/no-reserved-component-names': [
+      'error',
+      { disallowVue3BuiltInComponents: true },
+    ],
+    'vue/no-template-target-blank': 'error',
+    'vue/no-useless-mustaches': 'error',
+    'vue/no-useless-v-bind': 'error',
+    'vue/padding-line-between-blocks': 'error',
+    'vue/valid-define-emits': 'error',
+    'vue/valid-define-props': 'error',
+    'vue/valid-v-memo': 'error',
   },
   overrides: [
     {
